d808f1cf6d6e1a9b23f5903ccf81ec6b27af9b6c
Juciara-Silva <juciara2357@gmail.com>
2025-09-24 17:03:16 -0300

    .

---
+++ binario para string/ques0.py
@@ -0,0 +1,19 @@
+import sys
+
+try:
+   intValor = int(input('\nDigite um número inteiro: '))
+except ValueError:
+   sys.exit('Valor inválido. Por favor, insira um número inteiro.')
+except KeyboardInterrupt:
+   sys.exit('\nPrograma interrompido pelo usuário.')
+except Exception as erro:
+   sys.exit(f'Erro inesperado: {erro}')
+else:
+   # Imprimir o valor binário do número inteiro
+   print(f'\n{intValor} em Binário....: {bin(intValor)}')
+
+   # Imprimir o valor hexadecimal do número inteiro
+   print(f'{intValor} em Hexadecimal: {hex(intValor)}')  
+
+   # Imprimir o valor octal do número inteiro
+   print(f'{intValor} em Octal......: {oct(intValor)}\n')
